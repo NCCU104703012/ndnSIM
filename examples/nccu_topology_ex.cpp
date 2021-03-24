56acb5953d0c7736545ccd52267694270ddab933
nccu108753108 <wade851028@gmail.com>
2021-03-24 16:11:56 +0800

    nccu_topo_ex 1 and topology add

--- examples/nccu_topology_ex.cpp
+++ examples/nccu_topology_ex.cpp
@@ -17,25 +17,19 @@
  * ndnSIM, e.g., in COPYING.md file.  If not, see <http://www.gnu.org/licenses/>.
  **/
 
-// ndn-grid-multiple-strategies.cpp
+// ndn-grid-topo-plugin.cpp
 
 #include "ns3/core-module.h"
 #include "ns3/network-module.h"
-#include "ns3/point-to-point-module.h"
-#include "ns3/point-to-point-layout-module.h"
 #include "ns3/ndnSIM-module.h"
 
-using namespace ns3;
-using ns3::ndn::StackHelper;
-using ns3::ndn::AppHelper;
-using ns3::ndn::GlobalRoutingHelper;
-using ns3::ndn::StrategyChoiceHelper;
+// for LinkStatusControl::FailLinks and LinkStatusControl::UpLinks
+#include "ns3/ndnSIM/helper/ndn-link-control-helper.hpp"
+
+namespace ns3 {
 
 /**
- * This scenario simulates a grid topology (using PointToPointGrid module)
- *
- * The first six nodes use the best route forwarding strategy, whereas
- * the three remaining nodes use the multicast forwarding strategy.
+ * This scenario simulates a grid topology (using topology reader module)
  *
  * (consumer) -- ( ) ----- ( )
  *     |          |         |
@@ -47,7 +41,7 @@ using ns3::ndn::StrategyChoiceHelper;
  *
  * FIB is populated using NdnGlobalRoutingHelper.
  *
- * Consumer requests data from producer with frequency 100 interests per second
+ * Consumer requests data from producer with frequency 10 interests per second
  * (interests contain constantly increasing sequence number).
  *
  * For every received interest, producer replies with a data packet, containing
@@ -55,73 +49,73 @@ using ns3::ndn::StrategyChoiceHelper;
  *
  * To run scenario and see what is happening, use the following command:
  *
- *     NS_LOG=ndn.Consumer:ndn.Producer ./waf --run=ndn-grid-multiple-strategies
+ *     NS_LOG=ndn.Consumer:ndn.Producer ./waf --run=nccu_topology_ex
  */
 
 int
 main(int argc, char* argv[])
 {
-  // Setting default parameters for PointToPoint links and channels
-  Config::SetDefault("ns3::PointToPointNetDevice::DataRate", StringValue("1Mbps"));
-  Config::SetDefault("ns3::PointToPointChannel::Delay", StringValue("10ms"));
-  Config::SetDefault("ns3::QueueBase::MaxSize", StringValue("10p"));
-
-  // Read optional command-line parameters (e.g., enable visualizer with ./waf --run=<> --visualize
   CommandLine cmd;
   cmd.Parse(argc, argv);
 
-  // Creating 3x3 topology
-  PointToPointHelper p2p;
-  PointToPointGridHelper grid(3, 3, p2p);
-  grid.BoundingBox(100, 100, 200, 200);
+  AnnotatedTopologyReader topologyReader("", 25);
+  topologyReader.SetFileName("src/ndnSIM/examples/topologies/nccu_topo.txt");
+  topologyReader.Read();
 
   // Install NDN stack on all nodes
-  StackHelper ndnHelper;
+  ndn::StackHelper ndnHelper;
   ndnHelper.InstallAll();
 
+  // Set BestRoute strategy
+  ndn::StrategyChoiceHelper::InstallAll("/", "/localhost/nfd/strategy/multicast");
+
   // Installing global routing interface on all nodes
-  GlobalRoutingHelper ndnGlobalRoutingHelper;
+  ndn::GlobalRoutingHelper ndnGlobalRoutingHelper;
   ndnGlobalRoutingHelper.InstallAll();
 
   // Getting containers for the consumer/producer
-  Ptr<Node> producer = grid.GetNode(2, 2);
+  Ptr<Node> producer = Names::Find<Node>("Node8");
+  Ptr<Node> producer2 = Names::Find<Node>("Node9");
   NodeContainer consumerNodes;
-  consumerNodes.Add(grid.GetNode(0, 0));
+  consumerNodes.Add(Names::Find<Node>("Node0"));
 
   // Install NDN applications
   std::string prefix = "/prefix";
 
-  // Install different forwarding strategies
-  for (int row = 0; row < 3; row++) {
-    for (int column = 0; column < 3; column++) {
-      if (row < 2)
-        StrategyChoiceHelper::Install(grid.GetNode(row, column), "/prefix",
-                                      "/localhost/nfd/strategy/best-route");
-      else
-        StrategyChoiceHelper::Install(grid.GetNode(row, column), "/prefix",
-                                      "/localhost/nfd/strategy/multicast");
-    }
-  }
-
-  AppHelper consumerHelper("ns3::ndn::ConsumerCbr");
+  ndn::AppHelper consumerHelper("ns3::ndn::ConsumerCbr");
   consumerHelper.SetPrefix(prefix);
   consumerHelper.SetAttribute("Frequency", StringValue("100")); // 100 interests a second
   consumerHelper.Install(consumerNodes);
 
-  AppHelper producerHelper("ns3::ndn::Producer");
+  ndn::AppHelper producerHelper("ns3::ndn::Producer");
   producerHelper.SetPrefix(prefix);
   producerHelper.SetAttribute("PayloadSize", StringValue("1024"));
   producerHelper.Install(producer);
+  producerHelper.Install(producer2);
 
   // Add /prefix origins to ndn::GlobalRouter
   ndnGlobalRoutingHelper.AddOrigins(prefix, producer);
+  ndnGlobalRoutingHelper.AddOrigins(prefix, producer2);
 
   // Calculate and install FIBs
-  GlobalRoutingHelper::CalculateRoutes();
+  ndn::GlobalRoutingHelper::CalculateRoutes();
+
+  Ptr<Node> Fail_node1 = Names::Find<Node>("Node2");
+  Ptr<Node> Fail_node2 = Names::Find<Node>("Node9");
+  Simulator::Schedule(Seconds(10.0), ndn::LinkControlHelper::FailLink, Fail_node1, Fail_node2);
 
   Simulator::Stop(Seconds(20.0));
+
   Simulator::Run();
   Simulator::Destroy();
 
   return 0;
+}
+
+} // namespace ns3
+
+int
+main(int argc, char* argv[])
+{
+  return ns3::main(argc, argv);
 }
